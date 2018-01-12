55d541dd54d0714b76ec9ea1cafa58932a4048fa
Jakub Zíka <kuba.zika@email.cz>
2018-01-12 04:26:47 +0100

    Add index.js and index.html

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Client from './client';
+import Server from './server';
+// import Old from './old';
+
+let signallingServerURL = 'ws://localhost:8002/';
+let iceServerConfig = {
+  iceServers: [
+    {
+      urls: 'stun:stun.l.google.com:19302',
+    },
+    {
+      urls: 'stun:stun.services.mozilla.org'
+    }
+  ]
+};
+
+function isServer() {
+  const server = new Server(signallingServerURL, iceServerConfig, 8);
+  server.listen();
+  let sendButton = document.getElementById('send');
+  sendButton.onclick = () => {
+    server.send(document.getElementById('input').value,Object.keys(server.clients)[0]);
+  };
+}
+
+async function isClient() {
+  const client = new Client(signallingServerURL, iceServerConfig);
+  client.onopen = () => {
+  };
+  let connectButton = document.getElementById('connect');
+  connectButton.onclick = () => {
+    client.connect(document.getElementById('room').value);
+  };
+  let sendButton = document.getElementById('send');
+  sendButton.onclick = () => {
+    client.send(document.getElementById('input').value);
+  };
+}
+
+if(location.hash === '#s') {
+  isServer();
+} else {
+  isClient();
+}

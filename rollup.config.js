92de622f7ac1dede594d1cf86c6c647e9a959b5f
Jakub Zíka <kuba.zika@email.cz>
2018-01-12 04:24:15 +0100

    Update package.json and rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 export default {
   input: 'src/index.js',
   output: {
     format: 'iife',
     file: 'build/bundle.js',
     sourcemap: 'inline',
-  }
+  },
+  plugins: [
+    commonjs(),
+    resolve(),
+  ],
+  globals: ['io']
 };

7999b3be50ba361864966bd70163f586d21c795b
Jakub Zika <kuba.zika@email.cz>
2018-03-15 03:32:11 +0100

    Adjust rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,55 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-export default {
+import babel from 'rollup-plugin-babel'
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'iife',
+        file: 'build/opeteh.umd.js',
+        sourcemap: 'inline',
+        format: 'umd'
+      },
+    ],
+    moduleName: 'Opeteh',
+    plugins: [
+      resolve({
+        jsnext: true,
+        main: true,
+        module: true
+      }),
+      commonjs({
+        namedExports: {
+          'node_modules/lodash/lodash.js': [
+            'unionBy',
+            'union',
+            'reduce',
+            'find',
+            'forEach',
+            'includes',
+            'endsWith',
+            'remove',
+            'filter',
+            'concat',
+            'mapValues',
+            'pickBy',
+          ]
+        }
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+    ],
+    globals: ['io', 'lodash']
+},
+{
   input: 'src/index.js',
-  output: {
-    format: 'iife',
-    file: 'build/bundle.js',
-    sourcemap: 'inline',
-  },
-  plugins: [
-    commonjs(),
-    resolve(),
-  ],
-  globals: ['io']
-};
+  external: ['ms'],
+  output: [
+    { file: 'build/opeteh.cjs.js', format: 'cjs' },
+    { file: 'build/opeteh.es.js', format: 'es' }
+  ]
+}
+];

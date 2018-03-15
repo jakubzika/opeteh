70e8fe1548f786680ca54973d56a938ea6e4ee09
Jakub Zika <kuba.zika@email.cz>
2018-03-15 03:34:07 +0100

    Adjust server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import _ from 'lodash';
+import { forEach, mapValues, filter, pickBy } from 'lodash';
 
 import SignallingService from './signalling';
-import { status } from "./constants";
+import { STATUS, MESSAGE_TYPES, TYPE } from "./constants";
+import { resolvePromises, rejectPromises, addPromise } from './lib';
 
 function log(type, message, data) {
   console.log(`[${type}] - ${message}`, data);
@@ -21,28 +22,75 @@ class Server {
     this.onIceCandidate = this.onIceCandidate.bind(this);
     this.onMessage = this.onMessage.bind(this);
     this.onDataChannel = this.onDataChannel.bind(this);
+
+    this.promises = {
+      initialization: [],
+      newClient: [],
+    }
   }
+
+  // promise events
+  addPromise(type) {
+    return new Promise((resolve, reject) => {
+      this.promises = addPromise(this.promises, type, resolve, reject);
+    })
+  }
+
+  addClientPromise(clientId, type) {
+    return new Promise((resolve, reject) => {
+      this.clients[clientId].promises = addPromise(this.clients[clientId].promises, type, resolve, reject);
+    })
+  }
+
+
+  resolveClientPromise(clientId, type, data) {
+    this.clients[clientId].promises = resolvePromises(this.clients[clientId].promises, type, data)
+  }
+
+  resolvePromises(type, data) {
+    this.promises = resolvePromises(this.promises, type, data);
+  }
+
+  initialization() {
+    if (!this.room) {
+      return this.addPromise('initialization')
+    }
+  }
+
+  newClient() {
+    return this.addPromise('newClient')
+  }
+
+  clientInfo(from) {
+    return this._receive(from, MESSAGE_TYPES.CLIENT_INFO)
+  }
+  //
 
   get activeClients() {
     let numOfActiveClients = 0;
-    _.forEach(this.clients, (client) => {
-      if(client.state !== status.DISCONNECTED) {
+    forEach(this.clients, (client) => {
+      if (client.state !== STATUS.DISCONNECTED) {
         numOfActiveClients++;
       }
     });
     return numOfActiveClients;
   }
 
+  get clientsInfo() {
+    return mapValues(this.clients, (client, clientId) => {
+      return {
+        info: client.info,
+      }
+    })
+  }
+
   onSendChannelOpen(clientId) {
     return (evt) => {
       log('send channel', 'send channel open');
-      this.send('open', true, clientId);
       this.clients[clientId].sendChannelOpen = true;
-      if(this.clients[clientId].receiveChannelOpen && !this.clients[clientId].connected) {
+      if (this.clients[clientId].receiveChannelOpen && !this.clients[clientId].connected) {
         this.clients[clientId].connected = true;
-        if(typeof this.onopen === 'function') {
-          this.onopen(clientId);
-        }
+        this.resolvePromises('newClient', clientId)
       }
     }
   }
@@ -51,18 +99,16 @@ class Server {
     return (evt) => {
       log('receive channel', 'receive channel open');
       this.clients[clientId].receiveChannelOpen = true;
-      if(this.clients[clientId].receiveChannelOpen && !this.clients[clientId].connected) {
+      if (this.clients[clientId].receiveChannelOpen && !this.clients[clientId].connected) {
         this.clients[clientId].connected = true;
-        if(typeof this.onopen === 'function') {
-          this.onopen(clientId);
-        }
+        this.resolvePromises('newClient', clientId)
       }
     }
   }
 
   onIceCandidate(clientId) {
     return (evt) => {
-      if(evt.candidate) {
+      if (evt.candidate) {
         log('ice', 'got ice candidate', evt.candidate);
         this.signalling.send('ice', evt.candidate, clientId);
       }
@@ -77,14 +123,57 @@ class Server {
     this.clients[clientId].peerConnection.addIceCandidate(message.data);
   }
 
+  processMessage(message, clientId) {
+    message.from = clientId;
+    this.resolvePromises('message', message);
+    this.resolvePromises(message.type, message);
+    if (message.type === MESSAGE_TYPES.DATA && message.customType) {
+      this.resolvePromises(`${message.type}/${message.customType}`, message);
+    }
+    if (this.clients[clientId]) {
+      this.resolveClientPromise(clientId, 'message', message);
+      this.resolveClientPromise(clientId, message.type, message);
+      if (message.type === MESSAGE_TYPES.DATA && message.customType) {
+        this.resolveClientPromise(clientId, `${message.type}/${message.customType}`, message)
+      }
+    }
+
+    switch (message.type) {
+      case MESSAGE_TYPES.OPEN:
+        break;
+      case MESSAGE_TYPES.DATA:
+        break;
+      case MESSAGE_TYPES.CLIENT_INFO:
+        this.clients[clientId].info = message.data;
+        
+        break;
+    }
+  }
+
   onMessage(clientId) {
     return (evt) => {
-      log('message', 'incoming message', JSON.parse(evt.data));
-      const message= JSON.parse(evt.data);
-      switch(message.type) {
-        case 'open':
+      const message = JSON.parse(evt.data);
 
-          break;
+      // Handle to who is message meant to
+      let to = message.to;
+
+      if (typeof(to) === 'string') {
+        if (to === TYPE.SERVER) {
+          this.processMessage(message, clientId)
+        }
+        else if (to === TYPE.BROADCAST) {
+          this.processMessage(message, clientId)
+          this._send(message.type, message.data, to, clientId)
+        } else {
+          this._send(message.type, message.data, to, clientId)
+        }
+      } else if (typeof(to) === 'object') {
+        if (to.includes(TYPE.SERVER)) {
+          this.processMessage(message, clientId);
+          this._send(message.type, message.data, to, clientId)
+        } else {
+          this._send(message.type, message.data, to, clientId)
+        }
       }
     };
   }
@@ -97,6 +186,7 @@ class Server {
       receiveChannel.onopen = this.onReceiveChannelOpen(clientId);
 
       this.clients[clientId].receiveChannel = receiveChannel;
+      window.receiveChannel = receiveChannel;
     }
   }
 
@@ -111,7 +201,7 @@ class Server {
     this.clients[clientId].sendChannel.onopen = this.onSendChannelOpen(clientId);
   }
 
-  close(id) {
+  close(clientId) {
     this.clients[clientId].peerConnection.close();
     this.clients[clientId].receiveChannel.close();
     this.clients[clientId].sendChannel.close();
@@ -120,16 +210,28 @@ class Server {
   async listen() {
     await this.signalling.isConnected();
     await this.signalling.start();
-    while(this.activeClients < this.maxConnections) {
+
+    this.room = this.signalling.room;
+    this.id = this.signalling.id;
+
+    this.resolvePromises('initialization', this.room);
+    while (this.activeClients < this.maxConnections) {
       log('initialization', 'waiting for initial request message');
       const initialMessage = await this.signalling.message('request');
       log('initialization', 'got initialization message', initialMessage);
-      const client = {};
+      const client = {
+        promises: {
+          message: [],
+        },
+        info: undefined,
+      };
       const id = initialMessage.from;
       this.clients[id] = client;
 
-      client.sendChannelConnected = false;
-      client.receiveChannelConnected = false;
+      // client.sendChannelConnected = false;
+      // client.receiveChannelConnected = false;
+
+
 
       client.peerConnection = new RTCPeerConnection(this.iceServers);
       log('peer connection', 'created RTCPeerConnection');
@@ -145,27 +247,72 @@ class Server {
       log('peer connection', 'set localDescription');
       this.signalling.send('response', answer, id);
       log('peer connection', 'sent response');
+
+      this._receive(id, TYPE.CLIENT_INFO)
+        .then((infoMessage) => {
+          console.log('received info message');
+          this.clients[id].info = infoMessage.data;
+          // TODO add resolve promise to let know that user is ready
+        })
+      await this.newClient();
+      
+      this._send(MESSAGE_TYPES.NETWORK_INFO, {
+        clients: pickBy(this.clientsInfo, (client, clientId) => {
+          return clientId !== id;
+        }),
+      }, id);
+
+      this._send(MESSAGE_TYPES.NEW_CONNECTION, {
+        id,
+      }, TYPE.BROADCAST, id);
     }
   }
 
-  send(type, data, id) {
+
+  _send(type, data, to, from = TYPE.SERVER, customType) {
     const message = {
       type,
       data,
+      from,
+      customType,
     };
-    this.clients[id].sendChannel.send(JSON.stringify(message));
-  }
-
-  receive(id) {
-    return new Promise((resolve, reject) => {
-      const evtListener = this.clients[id].receiveChannel.addEventListeners('message', (evt) => {
-        const message = JSON.parse(evt.data);
-        if(message.type === 'data') {
-          resolve(message.data);
-          this.clients[id].receiveChannel.removeEventListener('message', evtListener);
+    const serializedMessage = JSON.stringify(message);
+    if (typeof to === 'object') {
+      forEach(to, (recipient) => {
+        if (recipient !== TYPE.SERVER) {
+          this.clients[recipient].sendChannel.send(serializedMessage);
         }
       })
-    });
+    } else if (to == TYPE.BROADCAST) {
+      forEach(this.clients, (client, clientId) => {
+        if (from !== clientId) {
+          client.sendChannel.send(serializedMessage)
+        }
+      })
+    }
+    else {
+      this.clients[to].sendChannel.send(serializedMessage);
+    }
+  }
+
+  send(data, to, customType) {
+    this._send(MESSAGE_TYPES.DATA, data, to, TYPE.SERVER, customType)
+  }
+
+  _receive(from, type) {
+    if (from) {
+      return this.addClientPromise(from, type);
+    } else {
+      return this.addPromise(type);
+    }
+  }
+
+  receive(from, customType) {
+    if (customType) {
+      return this._receive(from, `${MESSAGE_TYPES.DATA}/${customType}`)
+    } else {
+      return this._receive(from, MESSAGE_TYPES.DATA)
+    }
   }
 }
 

53fd94cf925efc319cf92b741c49d9859fe12a62
Jakub Zika <kuba.zika@email.cz>
2018-03-15 03:32:44 +0100

    Adjust constants

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,33 @@
 
-export const status = {
+export const STATUS = {
   CONNECTED: 'CONNECTED',
   DISCONNECTED: 'DISCONNECTED',
   CONNECTING: 'CONNECTING',
 };
 
-export const type = {
+export const TYPE = {
   SERVER: 'SERVER',
   CLIENT: 'CLIENT',
+  BROADCAST: 'BROADCAST',
 };
 
-export const networkChange = {
+export const NETWORK_CHANGE = {
   NEW_CONNECTION: 'NEW_CONNECTION',
   CLIENT_DISCONNECTED: 'CLIENT_DISCONNECTED',
 };
+
+export const MESSAGE_TYPES = {
+  NEW_CONNECTION: 'NEW_CONNECTION',
+  CLIENT_DISCONNECTED: 'CLIENT_DISCONNECTED',
+  NEW_CLIENT: '',
+  DATA: 'DATA',
+  OPEN: 'OPEN',
+  CLIENT_INFO: 'CLIENT_INFO',
+  NETWORK_INFO: 'NETWORK_INFO',
+}
+
+export const SIGNALLING_MESSAGE_TYPES = {
+};
+
+
+

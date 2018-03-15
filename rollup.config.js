bfb237be69976c63ef052818bb528d769e50b1ec
Jakub Zika <kuba.zika@email.cz>
2018-03-15 22:31:04 +0100

    Create only one build

--- rollup.config.js
+++ rollup.config.js
@@ -3,53 +3,52 @@ import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 
 export default [
-  {
-    input: 'src/index.js',
-    output: [
-      {
-        format: 'iife',
-        file: 'build/opeteh.umd.js',
-        sourcemap: 'inline',
-        format: 'umd'
-      },
-    ],
-    moduleName: 'Opeteh',
-    plugins: [
-      resolve({
-        jsnext: true,
-        main: true,
-        module: true
-      }),
-      commonjs({
-        namedExports: {
-          'node_modules/lodash/lodash.js': [
-            'unionBy',
-            'union',
-            'reduce',
-            'find',
-            'forEach',
-            'includes',
-            'endsWith',
-            'remove',
-            'filter',
-            'concat',
-            'mapValues',
-            'pickBy',
-          ]
-        }
-      }),
-      babel({
-        exclude: 'node_modules/**'
-      }),
-    ],
-    globals: ['io', 'lodash']
-},
+//   {
+//     input: 'src/index.js',
+//     output: [
+//       {
+//         format: 'iife',
+//         file: 'build/opeteh.umd.js',
+//         format: 'umd'
+//       },
+//     ],
+//     moduleName: 'Opeteh',
+//     plugins: [
+//       resolve({
+//         jsnext: true,
+//         main: true,
+//         module: true
+//       }),
+//       commonjs({
+//         namedExports: {
+//           'node_modules/lodash/lodash.js': [
+//             'unionBy',
+//             'union',
+//             'reduce',
+//             'find',
+//             'forEach',
+//             'includes',
+//             'endsWith',
+//             'remove',
+//             'filter',
+//             'concat',
+//             'mapValues',
+//             'pickBy',
+//           ]
+//         }
+//       }),
+//       babel({
+//         exclude: 'node_modules/**'
+//       }),
+//     ],
+//     globals: ['io', 'lodash']
+// },
 {
   input: 'src/index.js',
   external: ['ms'],
   output: [
-    { file: 'build/opeteh.cjs.js', format: 'cjs' },
-    { file: 'build/opeteh.es.js', format: 'es' }
+    { file: 'build/index.js', format: 'cjs' },
+    // { file: 'build/opeteh.es.js', format: 'es' }
   ]
 }
 ];

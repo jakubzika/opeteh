5833ecb68c9be444b602de2d87bdc3d9c7c9c6fc
Jakub Zika <kuba.zika@email.cz>
2018-03-15 03:33:54 +0100

    Adjust index

--- src/index.js
+++ src/index.js
@@ -1,45 +1,7 @@
 import Client from './client';
 import Server from './server';
-import Opeteh from './opeteh';
-// import Old from './old';
 
-let signallingServerURL = 'ws://localhost:8002/';
-let iceServerConfig = {
-  iceServers: [
-    {
-      urls: 'stun:stun.l.google.com:19302',
-    },
-    {
-      urls: 'stun:stun.services.mozilla.org'
-    }
-  ]
-};
-
-function isServer() {
-  const server = new Server(signallingServerURL, iceServerConfig, 8);
-  server.listen();
-  let sendButton = document.getElementById('send');
-  sendButton.onclick = () => {
-    server.send('message',document.getElementById('input').value, Object.keys(server.clients)[0]);
-  };
-}
-
-async function isClient() {
-  const client = new Client(signallingServerURL, iceServerConfig);
-  client.onopen = () => {
-  };
-  let connectButton = document.getElementById('connect');
-  connectButton.onclick = () => {
-    client.connect(document.getElementById('room').value);
-  };
-  let sendButton = document.getElementById('send');
-  sendButton.onclick = () => {
-    client.send('message',document.getElementById('input').value);
-  };
-}
-
-if (location.hash === '#s') {
-  isServer();
-} else {
-  isClient();
+export default {
+  OpetehServer: Server,
+  OpetehClient: Client,
 }
